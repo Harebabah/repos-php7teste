a30a388ea0b714ba3d37b6778d12571f13558a05
Harebabah <40242673+Harebabah@users.noreply.github.com>
2018-06-14 16:22:41 -0300

    Adicão de um echo ver teste no pc

--- prime.php
+++ prime.php
@@ -3,6 +3,7 @@
 echo "My first code php7! <strong>Olá Mundão</strong>";
 // agora usar (interrogação maior) não é obrigatório para fechar o bloco se o scritp só tem php. Se tem html e outros é importante limitar o que é o quê.
 #algumas tags também cairam no desuso no php7.
+echo "teste adicionar e mudança no projeto no pc"
 ?>
 
 
