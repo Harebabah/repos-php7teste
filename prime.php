2eecb3876f292daf04e491a119c6b1814821699d
Gerson Silva <fenoguru@gmail.com>
2018-06-14 14:54:56 -0300

    repositorio1

---
+++ prime.php
@@ -0,0 +1,9 @@
+<?php
+
+echo "My first code php7! <strong>Hello Word</strong>";
+// agora usar (interrogação maior) não é obrigatório para fechar o bloco se o scritp só tem php. Se tem html e outros é importante limitar o que é o quê.
+#algumas tags também cairam no desuso no php7.
+?>
+
+
+
