316bf30f045851e93046ced4768f673347a87666
Harebabah <40242673+Harebabah@users.noreply.github.com>
2018-06-14 16:08:44 -0300

    tradução de parte do texto

--- prime.php
+++ prime.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "My first code php7! <strong>Hello Word</strong>";
+echo "My first code php7! <strong>Olá Mundão</strong>";
 // agora usar (interrogação maior) não é obrigatório para fechar o bloco se o scritp só tem php. Se tem html e outros é importante limitar o que é o quê.
 #algumas tags também cairam no desuso no php7.
 ?>
